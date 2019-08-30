7b1c5ef6e4c5a2d582e31884b4ee11373e196d50
JackDesBwa <jackdesbwa@gmail.com>
2019-08-30 14:05:00 +0200

    Add Simplest Color Balance algorithm

--- code/stereomaton.py
+++ code/stereomaton.py
@@ -53,6 +53,19 @@ class ClickListener:
                     except BlockingIOError:
                         pass
 
+"""Apply Simplest Color Balance algorithm
+Reimplemented based on https://gist.github.com/DavidYKay/9dad6c4ab0d8d7dbf3dc"""
+def simplest_cb(img, percent):
+    out_channels = []
+    channels = cv2.split(img)
+    totalstop = channels[0].shape[0] * channels[0].shape[1] * percent / 200.0
+    for channel in channels:
+        bc = np.bincount(channel.ravel(), minlength=256)
+        lv = np.searchsorted(np.cumsum(bc), totalstop)
+        hv = 255-np.searchsorted(np.cumsum(bc[::-1]), totalstop)
+        out_channels.append(cv2.LUT(channel, np.array(tuple(0 if i < lv else 255 if i > hv else round((i-lv)/(hv-lv)*255) for i in np.arange(0, 256)), dtype="uint8")))
+    return cv2.merge(out_channels)
+
 """Clear screen"""
 def clear(cr):
   cr.set_source_rgb(0, 0, 0)
@@ -192,6 +205,11 @@ def photo_compute(code, nb):
         imglc = cv2.warpPerspective(imgl, Hl, sz)
         imgrc = cv2.warpPerspective(imgr, Hr, sz)
 
+        draw_countdown(cr, -2)
+        imglc = simplest_cb(imglc, 1)
+        imgrc = simplest_cb(imgrc, 1)
+
+        draw_countdown(cr, -1)
         clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(64, 64))
         labl = cv2.cvtColor(imglc, cv2.COLOR_BGR2LAB)
         labr = cv2.cvtColor(imgrc, cv2.COLOR_BGR2LAB)
