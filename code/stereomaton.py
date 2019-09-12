674d8bdb9391b5a1e88d6c26116e302719dfb39a
JackDesBwa <jackdesbwa@gmail.com>
2019-09-12 16:25:38 +0200

    Replace white balance algorithm by a faster implementation

--- code/stereomaton.py
+++ code/stereomaton.py
@@ -55,16 +55,22 @@ class ClickListener:
 
 """Apply Simplest Color Balance algorithm
 Reimplemented based on https://gist.github.com/DavidYKay/9dad6c4ab0d8d7dbf3dc"""
-def simplest_cb(img, percent):
-    out_channels = []
-    channels = cv2.split(img)
-    totalstop = channels[0].shape[0] * channels[0].shape[1] * percent / 200.0
-    for channel in channels:
-        bc = np.bincount(channel.ravel(), minlength=256)
-        lv = np.searchsorted(np.cumsum(bc), totalstop)
-        hv = 255-np.searchsorted(np.cumsum(bc[::-1]), totalstop)
-        out_channels.append(cv2.LUT(channel, np.array(tuple(0 if i < lv else 255 if i > hv else round((i-lv)/(hv-lv)*255) for i in np.arange(0, 256)), dtype="uint8")))
-    return cv2.merge(out_channels)
+def simplest_cb(img, percent=1):
+  out_channels = []
+  cumstops = (
+    img.shape[0] * img.shape[1] * percent / 200.0,
+    img.shape[0] * img.shape[1] * (1 - percent / 200.0)
+  )
+  for channel in cv2.split(img):
+    cumhist = np.cumsum(cv2.calcHist([channel], [0], None, [256], (0,256)))
+    low_cut, high_cut = np.searchsorted(cumhist, cumstops)
+    lut = np.concatenate((
+      np.zeros(low_cut),
+      np.around(np.linspace(0, 255, high_cut - low_cut + 1)),
+      255 * np.ones(255 - high_cut)
+    ))
+    out_channels.append(cv2.LUT(channel, lut.astype('uint8')))
+  return cv2.merge(out_channels)
 
 """Clear screen"""
 def clear(cr):
